a8309a0524ce5cef4bd1a33b6ef99fbe998414dd
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2018-11-01 12:28:39 +0100

    Corrected calculation of number of tiles from tile and total resolution.

--- src/datatypes/descriptor.cpp
+++ src/datatypes/descriptor.cpp
@@ -19,8 +19,12 @@ Descriptor::Descriptor(std::function<UniqueRaster(const Descriptor &)> &&getter,
         : getter(std::move(getter)), totalInfo(qrect_total), tileInfo(qrect_tile) { }
 
 uint64_t Descriptor::tilesOfRaster() const {
-    uint64_t  x = totalInfo.res_x / tileInfo.res_x + totalInfo.res_x % tileInfo.res_x;
-    uint64_t  y = totalInfo.res_y / tileInfo.res_y + totalInfo.res_y % tileInfo.res_y;
+    uint64_t x = totalInfo.res_x / tileInfo.res_x;
+    uint64_t y = totalInfo.res_y / tileInfo.res_y;
+    if(totalInfo.res_x % tileInfo.res_x > 0)
+        x += 1;
+    if(totalInfo.res_y % tileInfo.res_y > 0)
+        y += 1;
     return x * y;
 }
 
