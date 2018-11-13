c8c4a79522003f789874a672e4027f25565f76b1
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2018-11-13 12:30:44 +0100

    Projection class and moved definitions from all the spatial temporal info classes into cpp file.

---
+++ src/datatypes/spatial_temporal_reference.cpp
@@ -0,0 +1,119 @@
+
+#include "datatypes/spatial_temporal_reference.h"
+#include "spatial_temporal_reference.h"
+
+using namespace rts;
+
+// Projection definitions:
+
+Projection::Projection() : code(4326), authority("EPSG") { }
+
+Projection::Projection(std::string authority, uint32_t code) : authority(std::move(authority)), code(code) { }
+
+SpatialReference Projection::getExtent() {
+    //copied from mapping
+    if (authority == "EPSG" && code == 3857)
+        return SpatialReference(-20037508.34,-20037508.34,20037508.34,20037508.34);
+    else if (authority == "EPSG" && code == 4326)
+        return SpatialReference(-180, -90, 180, 90);
+    else if (authority == "SR-ORG" && code == 81 )
+        return SpatialReference(-5568748.276, -5568748.276, 5568748.276, 5568748.276);
+    else
+        return SpatialReference(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+}
+
+// Temporal Reference definitions:
+
+TemporalReference::TemporalReference(double t1, double t2) : t1(t1), t2(t2) { }
+
+TemporalReference::TemporalReference(const Json::Value &temp) {
+    t1 = temp["start"].asDouble();
+    t2 = temp["end"].asDouble();
+}
+TemporalReference::TemporalReference() : t1(0.0), t2(0.0) { }
+
+TemporalReference TemporalReference::getOverlapTemporal(const TemporalReference &o) const {
+    return TemporalReference(t1 > o.t1 ? t1 : o.t1, t2 < o.t2 ? t2 : o.t2);
+}
+bool TemporalReference::overlapsWithTemporal(const TemporalReference &other) const {
+    return t1 <= other.t2 && t2 >= other.t1;
+}
+
+// Spatial Reference definitions:
+
+SpatialReference::SpatialReference(double x1, double x2, double y1, double y2) : x1(x1), x2(x2), y1(y1), y2(y2), projection() { }
+
+SpatialReference::SpatialReference() : x1(-180), x2(180), y1(-90), y2(90), projection() { }
+
+SpatialReference::SpatialReference(const Json::Value &spat) : projection() {
+    x1 = spat["x1"].asDouble();
+    x2 = spat["x2"].asDouble();
+    y1 = spat["y1"].asDouble();
+    y2 = spat["y2"].asDouble();
+}
+
+bool SpatialReference::equalsSpatial(const SpatialReference &other) const {
+    //TODO: add delta for float inaccuracy?
+    return x1 == other.x1 && x2 == other.x2 && y1 == other.y1 && y2 == other.y2;
+}
+
+bool SpatialReference::overlapsWithSpatial(const SpatialReference &other) const {
+    return x1 <= other.x2 && x2 >= other.x1 && y1 <= other.y2 && y2 >= other.y1;
+}
+
+SpatialReference SpatialReference::getOverlapSpatial(const SpatialReference &other) const {
+    if(!overlapsWithSpatial(other)){
+        return SpatialReference(0,0,0,0);
+    }
+    double xx1 = x1 > other.x1 ? x1 : other.x1;
+    double xx2 = x2 < other.x2 ? x2 : other.x2;
+    double yy1 = y1 > other.y1 ? y1 : other.y1;
+    double yy2 = y2 < other.y2 ? y2 : other.y2;
+    return SpatialReference(xx1, xx2, yy1, yy2);
+}
+
+// Resolution definitions:
+
+Resolution::Resolution(uint32_t x, uint32_t y) : res_x(x), res_y(y) { }
+
+Resolution::Resolution() : res_x(0), res_y(0) { }
+
+Resolution::Resolution(const Json::Value &res){
+    res_x = res["x"].asUInt();
+    res_y = res["y"].asUInt();
+}
+
+bool Resolution::equalsResolution(const Resolution &other) const {
+    return res_x == other.res_x && res_y == other.res_y;
+}
+
+// Spatial Temporal Reference definitions:
+
+SpatialTemporalReference::SpatialTemporalReference(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y)
+        : TemporalReference(t1, t2), SpatialReference(x1, x2, y1, y2), Resolution(res_x, res_y) { }
+
+SpatialTemporalReference::SpatialTemporalReference(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res)
+        : TemporalReference(temp_ref), SpatialReference(spat_ref), Resolution(res) { }
+
+SpatialTemporalReference::SpatialTemporalReference(const Json::Value &qrect)
+        : TemporalReference(qrect["temporal_reference"]),SpatialReference(qrect["spatial_reference"]), Resolution(qrect["resolution"]) { }
+
+// Query Rectangle defintions:
+
+QueryRectangle::QueryRectangle(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y, Order order)
+        : SpatialTemporalReference(t1,t2, x1, x2, y1, y2, res_x, res_y), order(order) { }
+
+QueryRectangle::QueryRectangle(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res, Order order)
+        : SpatialTemporalReference(temp_ref, spat_ref, res), order(order) { }
+
+QueryRectangle::QueryRectangle(const Json::Value &qrect)
+        : SpatialTemporalReference(qrect)
+{
+    std::string order_str = qrect["order"].asString();
+    if(order_str == "SpatialTemporal")
+        order = Order::SpatialTemporal;
+    else if(order_str == "TemporalSpatial")
+        order = Order::TemporalSpatial;
+    else
+        throw std::runtime_error("Invalid value for order enum: " + order_str);
+}
--- src/datatypes/spatial_temporal_reference.h
+++ src/datatypes/spatial_temporal_reference.h
@@ -9,106 +9,73 @@
 
 namespace rts {
 
+    class SpatialReference;
+
+    class Projection {
+    public:
+        //TODO: not handling projection right at the moment, for now everything is EPSG:4326. fix that.
+        Projection();
+        Projection(std::string authority, uint32_t code);
+        std::string authority;
+        uint32_t code;
+
+        /**
+         * @return The coordinates maximum extent of this projection.
+         */
+        SpatialReference getExtent();
+    };
+
     class TemporalReference {
     public:
-        TemporalReference(double t1, double t2) : t1(t1), t2(t2) { }
-        TemporalReference(const Json::Value &temp) {
-            t1 = temp["start"].asDouble();
-            t2 = temp["end"].asDouble();
-        }
-        TemporalReference() : t1(0.0), t2(0.0) { }
-        TemporalReference getOverlapTemporal(const TemporalReference &o){
-            return TemporalReference(t1 > o.t1 ? t1 : o.t1, t2 < o.t2 ? t2 : o.t2);
-        }
-        bool overlapsWithTemporal(const TemporalReference &other){
-            return t1 <= other.t2 && t2 >= other.t1;
-        }
+        TemporalReference();
+        TemporalReference(double t1, double t2);
+        explicit TemporalReference(const Json::Value &temp);
+        TemporalReference getOverlapTemporal(const TemporalReference &o) const;
+        bool overlapsWithTemporal(const TemporalReference &other) const;
+
         double t1;
         double t2;
     };
 
     class SpatialReference {
     public:
-        SpatialReference(double x1, double x2, double y1, double y2) : x1(x1), x2(x2), y1(y1), y2(y2) { }
-        SpatialReference() : x1(-180), x2(180), y1(-90), y2(90) { }
-        SpatialReference(const Json::Value &spat){
-            x1 = spat["x1"].asDouble();
-            x2 = spat["x2"].asDouble();
-            y1 = spat["y1"].asDouble();
-            y2 = spat["y2"].asDouble();
-        }
-        bool equalsSpatial(const SpatialReference &other){
-            //TODO: add delta for float inaccuracy?
-            return x1 == other.x1 && x2 == other.x2 && y1 == other.y1 && y2 == other.y2;
-        }
-        SpatialReference getOverlapSpatial(const SpatialReference &other){
-            if(!overlapsWithSpatial(other)){
-                return SpatialReference(0,0,0,0);
-            }
-            double xx1 = x1 > other.x1 ? x1 : other.x1;
-            double xx2 = x2 < other.x2 ? x2 : other.x2;
-            double yy1 = y1 > other.y1 ? y1 : other.y1;
-            double yy2 = y2 < other.y2 ? y2 : other.y2;
-            return SpatialReference(xx1, xx2, yy1, yy2);
-        }
-        bool overlapsWithSpatial(const SpatialReference &other){
-            return x1 <= other.x2 && x2 >= other.x1 && y1 <= other.y2 && y2 >= other.y1;
-        }
+        SpatialReference();
+        SpatialReference(double x1, double x2, double y1, double y2);
+        explicit SpatialReference(const Json::Value &spat);
+        bool overlapsWithSpatial(const SpatialReference &other) const;
+        bool equalsSpatial(const SpatialReference &other)  const;
+        SpatialReference getOverlapSpatial(const SpatialReference &other) const;
+
         double x1;
         double x2;
         double y1;
         double y2;
+        Projection projection;
     };
 
     class Resolution {
     public:
-        Resolution(uint32_t x, uint32_t y) : res_x(x), res_y(y) { }
-        Resolution() : res_x(0), res_y(0) { }
-        Resolution(const Json::Value &res){
-            res_x = res["x"].asUInt();
-            res_y = res["y"].asUInt();
-        }
-        bool equalsResolution(const Resolution &other) const {
-            return res_x == other.res_x && res_y == other.res_y;
-        }
+        Resolution(uint32_t x, uint32_t y);
+        Resolution();
+        explicit Resolution(const Json::Value &res);
+        bool equalsResolution(const Resolution &other) const;
         uint32_t res_x;
         uint32_t res_y;
     };
 
     class SpatialTemporalReference : public TemporalReference, public SpatialReference, public Resolution {
     public:
-        SpatialTemporalReference(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y)
-                : TemporalReference(t1, t2), SpatialReference(x1, x2, y1, y2), Resolution(res_x, res_y){ }
-
-        SpatialTemporalReference(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res)
-                : TemporalReference(temp_ref), SpatialReference(spat_ref), Resolution(res) { }
-
-        explicit SpatialTemporalReference(const Json::Value &qrect)
-                : TemporalReference(qrect["temporal_reference"]),
-                  SpatialReference(qrect["spatial_reference"]),
-                  Resolution(qrect["resolution"]) { }
+        SpatialTemporalReference(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y);
+        SpatialTemporalReference(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res);
+        explicit SpatialTemporalReference(const Json::Value &qrect);
         SpatialTemporalReference(const SpatialTemporalReference &other) = default;
     };
 
     class QueryRectangle : public SpatialTemporalReference {
     public:
-        QueryRectangle(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y, Order order)
-            : SpatialTemporalReference(t1,t2, x1, x2, y1, y2, res_x, res_y), order(order) { }
-
-        QueryRectangle(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res, Order order)
-            : SpatialTemporalReference(temp_ref, spat_ref, res), order(order) { }
-
-        explicit QueryRectangle(const Json::Value &qrect)
-                : SpatialTemporalReference(qrect)
-        {
-            std::string order_str = qrect["order"].asString();
-            if(order_str == "SpatialTemporal")
-                order = Order::SpatialTemporal;
-            else if(order_str == "TemporalSpatial")
-                order = Order::TemporalSpatial;
-            else
-                throw std::runtime_error("Invalid value for order enum: " + order_str);
-        }
+        QueryRectangle(double t1, double t2, double x1, double x2, double y1, double y2, uint32_t res_x, uint32_t res_y, Order order);
+        QueryRectangle(const TemporalReference &temp_ref, const SpatialReference &spat_ref, const Resolution &res, Order order);
+        explicit QueryRectangle(const Json::Value &qrect);
         QueryRectangle(const QueryRectangle &other) = default;
         Order order;
     };
