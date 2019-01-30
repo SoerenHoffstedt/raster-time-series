3572ad51b5c213f7112bfa84d6d6df366d7358d8
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2019-01-30 16:07:24 +0100

    Time interval: decrease function and comments added.

--- src/util/time_interval.cpp
+++ src/util/time_interval.cpp
@@ -1,6 +1,6 @@
 
-#include "util/time_interval.h"
 #include "util/parsing.h"
+#include "time_interval.h"
 
 using namespace rts;
 using namespace boost::posix_time;
@@ -14,25 +14,48 @@ TimeInterval::TimeInterval(const Json::Value &json) {
     length = json["length"].asUInt();
 }
 
-void TimeInterval::increase(boost::posix_time::ptime &time) const {
+void TimeInterval::increase(boost::posix_time::ptime &time, uint32_t times) const {
     switch(unit){
         case TimeUnit::Year:
-            time += boost::gregorian::years(length);
+            time += boost::gregorian::years(length * times);
             break;
         case TimeUnit::Month:
-            time += boost::gregorian::months(length);
+            time += boost::gregorian::months(length * times);
             break;
         case TimeUnit::Day:
-            time += boost::gregorian::days(length);
+            time += boost::gregorian::days(length * times);
             break;
         case TimeUnit::Hour:
-            time += boost::posix_time::hours(length);
+            time += boost::posix_time::hours(length * times);
             break;
         case TimeUnit::Minute:
-            time += boost::posix_time::minutes(length);
+            time += boost::posix_time::minutes(length * times);
             break;
         case TimeUnit::Second:
-            time += boost::posix_time::seconds(length);
+            time += boost::posix_time::seconds(length * times);
+            break;
+    }
+}
+
+void TimeInterval::decrease(boost::posix_time::ptime &time, uint32_t times) const {
+    switch(unit){
+        case TimeUnit::Year:
+            time -= boost::gregorian::years(length * times);
+            break;
+        case TimeUnit::Month:
+            time -= boost::gregorian::months(length * times);
+            break;
+        case TimeUnit::Day:
+            time -= boost::gregorian::days(length * times);
+            break;
+        case TimeUnit::Hour:
+            time -= boost::posix_time::hours(length * times);
+            break;
+        case TimeUnit::Minute:
+            time -= boost::posix_time::minutes(length * times);
+            break;
+        case TimeUnit::Second:
+            time -= boost::posix_time::seconds(length * times);
             break;
     }
 }
--- src/util/time_interval.h
+++ src/util/time_interval.h
@@ -22,16 +22,37 @@ namespace rts {
 
     /**
      * Class representing time intervals by a unit (year, month, day, ...) and a length. E.g. with the unit month
-     * and the length 3 the time interval represents steps of three months.
+     * and the length 3 the time interval represents time steps of three months.
      */
     class TimeInterval {
     public:
         TimeInterval();
         TimeInterval(TimeUnit unit, uint32_t length);
         explicit TimeInterval(const Json::Value &json);
+
+        /**
+         * The time unit of this interval, e.g. Month or Hour.
+         */
         TimeUnit unit;
+
+        /**
+         * How many units this interval covers.
+         */
         uint32_t length;
-        void increase(boost::posix_time::ptime &time) const;
+
+        /**
+         * Increase the passed time by the interval described by this object.
+         * @param time Reference to the time that is to be increased by this interval.
+         * @param times Factor to allow increasing multiple times with one call. Standard is 1.
+         */
+        void increase(boost::posix_time::ptime &time, uint32_t times = 1) const;
+
+        /**
+         * Decrease the passed time by the interval described by this object.
+         * @param time Reference to the time that is to be decreased by this interval.
+         * @param times Factor to allow decreasing multiple times with one call. Standard is 1.
+         */
+        void decrease(boost::posix_time::ptime &time, uint32_t times = 1) const;
     };
 
 }
