080e3f09d2251cfe1a76f318af746eaa4f302c18
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2019-02-01 15:57:12 +0100

    Benchmark utility class fleshed out.

--- src/util/benchmark.cpp
+++ src/util/benchmark.cpp
@@ -1,1 +1,51 @@
 
+#include "benchmark.h"
+
+using namespace rts;
+using namespace std::chrono;
+
+//init static members
+std::ofstream* Benchmark::outputFile = nullptr;
+high_resolution_clock::time_point Benchmark::queryStart;
+high_resolution_clock::time_point Benchmark::sourceStart;
+high_resolution_clock::time_point Benchmark::consumingStart;
+milliseconds Benchmark::sourceDuration(0);
+milliseconds Benchmark::consumingDuration(0);
+
+void Benchmark::setFileOutputStream(std::ofstream &outputFile) {
+    Benchmark::outputFile = &outputFile;
+}
+
+void Benchmark::endQuery() {
+    auto totalQuery = duration_cast<milliseconds>(high_resolution_clock::now() - queryStart).count();
+    (*outputFile) << sourceDuration.count() << std::endl;
+    (*outputFile) << consumingDuration.count() << std::endl;
+    (*outputFile) << totalQuery << std::endl;
+}
+
+void Benchmark::startSource() {
+    sourceStart = std::chrono::high_resolution_clock::now();
+}
+
+void Benchmark::startQuery() {
+    queryStart = std::chrono::high_resolution_clock::now();
+    sourceDuration = milliseconds(0);
+    consumingDuration = milliseconds(0);
+}
+
+void Benchmark::endSource() {
+    auto now = high_resolution_clock::now();
+    sourceDuration += duration_cast<milliseconds>(now - sourceStart);
+}
+
+void Benchmark::startConsuming() {
+    consumingStart = std::chrono::high_resolution_clock::now();
+}
+
+void Benchmark::endConsuming() {
+    auto now = high_resolution_clock::now();
+    consumingDuration += duration_cast<milliseconds>( now - consumingStart );
+}
+
+
+
--- src/util/benchmark.h
+++ src/util/benchmark.h
@@ -7,22 +7,33 @@
 
 namespace rts {
 
-    using std::chrono;
-
+    /**
+     * Utility class for benchmarking execution time of queries.
+     * Usage: set output file stream.
+     * Call startQuery() before start of the execution of the query.
+     * After execution of the query is finished, call endQuery().
+     * In the consuming operator call startConsuming() when entering and endConsuming() when exiting the consuming operator.
+     * The same applies for the source operator and start/endSource().
+     * endQuery() will write three lines into the output file. First line is the milliseconds spend in the source
+     * operator, second line is ms spend in consuming operator, and the third line is the total time spend in the query.
+     * The last number includes the time spend in source and consuming operator.
+     */
     class Benchmark {
     public:
-        static void startQuery(std::ifstream &fileOutput);
+        static void setFileOutputStream(std::ofstream &outputFile);
+        static void startQuery();
         static void endQuery();
         static void startSource();
         static void endSource();
         static void startConsuming();
         static void endConsuming();
     private:
-        high_resolution_clock queryStart;
-        high_resolution_clock sourceStart;
-        high_resolution_clock consumingStart;
-        duration<milliseconds> sourceDuration;
-        duration<milliseconds> consumingDuration;
+        static std::ofstream *outputFile;
+        static std::chrono::high_resolution_clock::time_point queryStart;
+        static std::chrono::high_resolution_clock::time_point sourceStart;
+        static std::chrono::high_resolution_clock::time_point consumingStart;
+        static std::chrono::milliseconds sourceDuration;
+        static std::chrono::milliseconds consumingDuration;
 
     };
 
