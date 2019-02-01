c2c4de738d9f675acf21acad664108e4039d6349
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2019-02-01 15:58:00 +0100

    Benchmark Query executable: Reading input query, output file, loop count from arguments. Using Benchmark class.

--- src/benchmark_query.cpp
+++ src/benchmark_query.cpp
@@ -3,13 +3,21 @@
 #include <iostream>
 #include <fstream>
 #include <json/json.h>
+#include <util/benchmark.h>
 #include "queries/operator_tree.h"
 #include "operators/consuming/print.h"
 #include "operators/expression_operator.h"
 #include "operators/source/fake_source.h"
 
-#define BENCHMARK
 
+/**
+ * Executes a query multiple times, tracks execution time for each query, and prints it into a file.
+ * Takes three input parameters: query file name, output file name, number of repeated execution.
+ * The output file contains three numbers for each query execution: source duration, consuming duration,
+ * and total query duration. Every number is written in a single liner and all numbers are in milliseconds.
+ * The three numbers of every query are simply written underneath each other without
+ * any differentiation except their line number.
+ */
 int main(int argc, char** argv) {
 
     using namespace rts;
@@ -19,19 +27,15 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    int count = 100;
+    int count = std::stoi(argv[2]);
 
-    std::string outputFilename = "benchmark_";
-    outputFilename += argv[1];
-    outputFilename += "_";
-    outputFilename += "..."; //TODO: time;
-    outputFilename += ".txt";
-
+    std::string outputFilename = argv[3];
     std::ofstream outputFile(outputFilename);
 
+    Benchmark::setFileOutputStream(outputFile);
+
     for(int i = 0; i < count; ++i){
-        std::string queryFilename("../../test/query/");
-        queryFilename += argv[1];
+        std::string queryFilename(argv[1]);
         std::ifstream file_in(queryFilename);
 
         Json::Value json_query;
@@ -39,14 +43,13 @@ int main(int argc, char** argv) {
 
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
+        Benchmark::startQuery();
+
         std::unique_ptr<OperatorTree> operatorTree = std::make_unique<OperatorTree>(json_query);
         std::unique_ptr<ConsumingOperator> p = operatorTree->instantiateConsuming();
         p->consume();
 
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1).count();
-        outputFile << duration << std::endl;
+        Benchmark::endQuery();
 
     }
     outputFile.close();
